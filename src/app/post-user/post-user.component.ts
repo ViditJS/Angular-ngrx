398906229407a24100a987b2dfa54c46d6fa925f
Your Name <you@example.com>
2020-06-28 07:48:36 -0700

    add post user

--- src/app/post-user/post-user.component.ts
+++ src/app/post-user/post-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Store, select } from '@ngrx/store';
+import * as fromUsers from '../store';
 
 @Component({
   selector: 'app-post-user',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<fromUsers.IUserState>) { }
+
+  FG = new FormGroup({
+    userId: new FormControl(101),
+    id: new FormControl(2000),
+    title: new FormControl('test title'),
+    body: new FormControl('test body')
+  });
+
+  isLoading: boolean = false;
 
   ngOnInit() {
+  }
+  public onSubmit() {
+    this.store.dispatch(new fromUsers.PostUser(this.FG.value));
+
+    const users$ = this.store.pipe(select(fromUsers.allUsers));
+
+    users$.subscribe(res => {
+      this.isLoading = res.isLoading;
+    });
   }
 
 }
